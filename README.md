0efda42e2d79d8cec300199696d0b52bd8512ad1
Adham <odinay@gmail.com>
2025-04-01 13:45:25 +1100

    moved data into yaml file, VIC checked

--- README.md
+++ README.md
@@ -5,3 +5,13 @@ A Python package to get school break dates for Australian states.
 ## Installation
 ```bash
 pip install ozschoolbreaks
+```
+
+## TODO
+- [ ] Check dates
+    - [x] VIC
+    - [ ] NSW
+    - [ ] QLD
+    - [ ] SA
+    - [ ] TAS
+- [ ] Add missing states
--- tests/test_breaks.py
+++ tests/test_breaks.py
@@ -1,13 +1,13 @@
 import pytest
 from datetime import date
-from ozschoolbreaks import get_breaks
+from ozschoolbreaks import get_breaks, BreakPeriod
 
 def test_get_breaks_valid():
     breaks = get_breaks("NSW")
     assert len(breaks) == 4
-    assert breaks[0]["term"] == 1
-    assert isinstance(breaks[0]["start"], date)
-    assert isinstance(breaks[0]["end"], date)
+    assert isinstance(breaks[0], BreakPeriod)
+    assert isinstance(breaks[0].start, date)
+    assert isinstance(breaks[0].end, date)
 
 def test_get_breaks_invalid_state():
     with pytest.raises(ValueError, match="Invalid state"):
@@ -16,4 +16,5 @@ def test_get_breaks_invalid_state():
 def test_get_breaks_year():
     breaks = get_breaks("NSW", year=2026)
     assert len(breaks) == 4
-    assert breaks[0]["start"].year == 2026
+    assert breaks[0].start.year == 2026
+    assert breaks[0].end.year == 2026
